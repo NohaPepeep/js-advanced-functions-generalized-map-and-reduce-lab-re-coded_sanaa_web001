ea502a02108867d9b518f729b0c3b1fb4c2bbe7b
Nuha Al-Sameai <noha.pepeep1@gmail.com>
2020-09-17 13:43:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,7 +6,11 @@ return newarr
 
 
 function reduce(sourceArray,fun, startingPoint=0){
+<<<<<<< HEAD
   let newarr;
+=======
+  const newarr;
+>>>>>>> 797070d92aa4de60b743910320c5280c0ede60df
   if(startingPoint!==0){
    newarr= sourceArray.reduce(fun,startingPoint);}
   else{
